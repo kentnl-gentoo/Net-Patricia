1b30274af0df37df843797386edbcb1eebd033eb
PHILIPP <philipp@cpan.org>
2009-11-16 02:00:22 +0000

    Import https://cpan.metacpan.org/authors/id/P/PH/PHILIPP/Net-Patricia-1.15_05.tar.gz

--- Patricia.pm
+++ Patricia.pm
@@ -39,7 +39,7 @@ BEGIN {
   @EXPORT = qw(AF_INET AF_INET6);
 }
 
-'$Revision: 1.15_04 $' =~ m/(\d+)\.(\d+)(_\d+|)/ && ( $VERSION = "$1.$2$3");
+'$Revision: 1.15_05 $' =~ m/(\d+)\.(\d+)(_\d+|)/ && ( $VERSION = "$1.$2$3");
 
 bootstrap Net::Patricia $VERSION;
 
